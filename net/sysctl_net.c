b0ba53d6223b64967051871b35a71abb95f636e0
bowenerchen <bowenerchen@tencent.com>
2020-05-28 00:12:18 +0800

    UDPATE

--- net/sysctl_net.c
+++ net/sysctl_net.c
@@ -39,7 +39,7 @@ static int is_seen(struct ctl_table_set *set)
 
 /* Return standard mode bits for table entry. */
 static int net_ctl_permissions(struct ctl_table_header *head,
-			       struct ctl_table *table)
+							   struct ctl_table *table)
 {
 	struct net *net = container_of(head->set, struct net, sysctls);
 	kuid_t root_uid = make_kuid(net->user_ns, 0);
@@ -47,12 +47,14 @@ static int net_ctl_permissions(struct ctl_table_header *head,
 
 	/* Allow network administrator to have same access as root. */
 	if (ns_capable_noaudit(net->user_ns, CAP_NET_ADMIN) ||
-	    uid_eq(root_uid, current_euid())) {
+		uid_eq(root_uid, current_euid()))
+	{
 		int mode = (table->mode >> 6) & 7;
 		return (mode << 6) | (mode << 3) | mode;
 	}
 	/* Allow netns root group to have the same access as the root group */
-	if (in_egroup_p(root_gid)) {
+	if (in_egroup_p(root_gid))
+	{
 		int mode = (table->mode >> 3) & 7;
 		return (mode << 3) | mode;
 	}
@@ -89,6 +91,7 @@ __init int net_sysctl_init(void)
 	 * registering "/proc/sys/net" as an empty directory not in a
 	 * network namespace.
 	 */
+	/*通过将 "/proc/sys/net" 注册为不在网络名称空间中的空目录来避免sysctl实现中的限制。*/
 	net_header = register_sysctl("net", empty);
 	if (!net_header)
 		goto out;
@@ -105,7 +108,7 @@ out1:
 }
 
 struct ctl_table_header *register_net_sysctl(struct net *net,
-	const char *path, struct ctl_table *table)
+											 const char *path, struct ctl_table *table)
 {
 	return __register_sysctl_table(&net->sysctls, path, table);
 }
