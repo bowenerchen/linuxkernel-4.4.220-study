74e7eaa45e2cac203f85a1b46ddf38cd87670d6a
bowenerchen <bowenerchen@tencent.com>
2021-01-03 22:35:30 +0800

    upgrade

---
+++ SourceCode/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP Sun Jan 3 22:28:12 CST 2021"
+#define LINUX_COMPILE_BY "bowenerchen"
+#define LINUX_COMPILE_HOST "dev"
+#define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
