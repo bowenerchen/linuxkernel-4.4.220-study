59897d56451c2586a424b26392bc52925cb24702
bowenerchen <bowenerchen@tencent.com>
2021-01-07 16:14:56 +0800

    upgrade running commands to running.sh

--- SourceCode/running.sh
+++ SourceCode/running.sh
@@ -5,6 +5,9 @@ qemu-system-arm \
 -smp 4 \
 -m 200M \
 -kernel arch/arm/boot/zImage \
--append "rdinit=/linuxrc console=ttyAMA0 loglevel=8" \
+-append "rdinit=/linuxrc console=ttyAMA0 loglevel=8 slub_debug kmemleak=on" \
 -dtb arch/arm/boot/dts/vexpress-v2p-ca9.dtb \
--nographic
+-nographic \
+--fsdev local,id=kmod_dev,path=$PWD/kmodules,security_model=none -device virtio-9p-device,fsdev=kmod_dev,mount_tag=kmod_mount 
+
+#-S -s
