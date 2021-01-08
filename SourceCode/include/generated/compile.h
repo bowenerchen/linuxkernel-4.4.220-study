66c09774e2a189b7135d2d1ddfb2595709fcd0b0
bowenerchen <bowenerchen@tencent.com>
2021-01-08 17:59:11 +0800

    upgrade .gitignore

--- SourceCode/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 7 */
-/* SMP */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP Fri Jan 8 16:22:46 CST 2021"
-#define LINUX_COMPILE_BY "bowenerchen"
-#define LINUX_COMPILE_HOST "dev"
-#define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
