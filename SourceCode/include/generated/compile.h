f2f7a7609a0d1caca2ee76d264dc8377ed76915d
bowenerchen <bowenerchen@tencent.com>
2021-01-08 16:31:25 +0800

    update

--- SourceCode/include/generated/compile.h
+++ SourceCode/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 7 */
 /* SMP */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#6 SMP Thu Jan 7 20:21:17 CST 2021"
+#define UTS_VERSION "#7 SMP Fri Jan 8 16:22:46 CST 2021"
 #define LINUX_COMPILE_BY "bowenerchen"
 #define LINUX_COMPILE_HOST "dev"
 #define LINUX_COMPILER "gcc version 5.5.0 (Linaro GCC 5.5-2017.10) "
