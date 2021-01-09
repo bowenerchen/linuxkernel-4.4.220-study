a4527bf8deeeee3f95b40c4c2f8829aa72b7bc95
bowenerchen <bowenerchen@tencent.com>
2021-01-09 17:39:37 +0800

    update

--- PrivateCodeSpace/char_device_module/main.c
+++ PrivateCodeSpace/char_device_module/main.c
@@ -4,10 +4,28 @@
 #include <linux/init.h>
 #include <linux/cdev.h>
 
+/***
+ * 字符设备驱动管理的核心对象是以字符为单位进行IO数据流传输的设备，常见的如键盘，鼠标，触摸屏等都属于字符设备
+ * struct cdev{} -- 对字符设备的抽象数据结构
+ * cdev_alloc() -- cdev实例化
+ * cdev_init() -- 初始化cdev，并建立设备与驱动操作方法集file_operations之间的关联关系
+ * cdev_add() -- 把一个字符设备添加到系统中，通常在驱动程序probe函数里会调用该接口来注册一个字符设备
+ * cdev_del() -- 从系统中删除一个字符设备，通常在驱动程序的卸载函数中会调用该接口
+ * alloc_chrdev_region() -- 自动为设备分配主设备号，避免主设备号冲突
+ * unregister_chedev_region() -- 归还设备的主设备号给系统
+ * 
+ * 用户自定义实现：
+ * static int demodrv_open()
+ * static int demodrv_release()
+ * static ssize_t demodrv_read()
+ * static ssize_t demodrv_write()
+ * 以上4个函数，为自定义实现的与用户态交互的逻辑
+ * ***/
+
 #define DEMO_NAME "my_demo_dev"
-static dev_t dev;
-static struct cdev *demo_cdev;
-static signed count = 1;
+static dev_t dev;              // 设备号
+static struct cdev *demo_cdev; // 指向设备实例的指针
+static signed count = 1;       // 同属一个主设备号的次设备号的个数，这里初始化为1
 
 static int demodrv_open(struct inode *inode, struct file *file)
 {
@@ -44,6 +62,7 @@ static const struct file_operations demodrv_fops = {
 static int __init simple_char_init(void)
 {
     int ret;
+    // 分配主设备号
     ret = alloc_chrdev_region(&dev, 0, count, DEMO_NAME);
     if (ret)
     {
@@ -51,6 +70,7 @@ static int __init simple_char_init(void)
         return ret;
     }
 
+    // 实例化cdev
     demo_cdev = cdev_alloc();
     if (!demo_cdev)
     {
@@ -58,8 +78,10 @@ static int __init simple_char_init(void)
         goto unregister_chrdev;
     }
 
+    // 初始化demo_cdev，将demo_cdev与回调函数进行绑定
     cdev_init(demo_cdev, &demodrv_fops);
 
+    // 注册该字符设备
     ret = cdev_add(demo_cdev, dev, count);
     if (ret)
     {
@@ -67,14 +89,16 @@ static int __init simple_char_init(void)
         goto cdev_fail;
     }
 
-    printk("succeed register char device: %s\n", DEMO_NAME);
+    printk("succeed register char device: %s, module name: %s, version: %s\n", DEMO_NAME, THIS_MODULE->name, THIS_MODULE->version);
     printk("Major number = %d, minor number = %d\n", MAJOR(dev), MINOR(dev));
     return 0;
 
 cdev_fail:
+    // 从系统中卸载字符设备
     cdev_del(demo_cdev);
 
 unregister_chrdev:
+    // 归还主设备号给系统
     unregister_chrdev_region(dev, count);
 
     return ret;
@@ -85,8 +109,10 @@ static void __exit simple_char_exit(void)
     printk("removing device\n");
     if (demo_cdev)
     {
+        // 从系统中卸载字符设备
         cdev_del(demo_cdev);
     }
+    // 归还主设备号给系统
     unregister_chrdev_region(dev, count);
 }
 
@@ -95,4 +121,5 @@ module_exit(simple_char_exit);
 
 MODULE_AUTHOR("bowenerchen");
 MODULE_LICENSE("GPL v2");
+MODULE_VERSION("bowenerchen-v1.0");
 MODULE_DESCRIPTION("simple character device");
--- SourceCode/include/linux/cdev.h
+++ SourceCode/include/linux/cdev.h
@@ -9,13 +9,14 @@ struct file_operations;
 struct inode;
 struct module;
 
-struct cdev {
-	struct kobject kobj;
-	struct module *owner;
-	const struct file_operations *ops;
-	struct list_head list;
-	dev_t dev;
-	unsigned int count;
+struct cdev
+{
+	struct kobject kobj;			   // linux设备驱动模型
+	struct module *owner;			   // 字符设备驱动程序所在的内核模块对象指针
+	const struct file_operations *ops; // 操作方法集，一般包含open，release，read，write等方法，和用户态程序交互时需要使用ops对上述方法进行注册
+	struct list_head list;			   // 将字符设备串成一个链表
+	dev_t dev;						   // 字符设备的设备号，由主设备号和次设备号组成，高12比特是主设备号，低20比特是次设备号
+	unsigned int count;				   // 同属一个主设备号的次设备号的个数
 };
 
 void cdev_init(struct cdev *, const struct file_operations *);
