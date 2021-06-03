f3232266285493a68904ea365297409a5445513f
bowenerchen <bowenerchen@tencent.com>
2021-06-03 17:21:33 +0800

    upgrade

--- SourceCode/build.sh
+++ SourceCode/build.sh
@@ -3,5 +3,5 @@
 export ARCH=arm
 export CROSS_COMPILE=arm-linux-gnueabi-
 cp ./config-template ./.config
-make -j8
-make dtbs
+make bzImage -j8 ARCH=arm CROSS_COMPILE=arm-linux-gnueabi-
+make dtbs -j8
